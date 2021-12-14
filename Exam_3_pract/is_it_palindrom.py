515b74dad1451359ad7f7bce8ef351e1385b9ab7
Pavel <pavel.krivoshei.1988@gmail.com>
2021-12-14 16:37:20 +0300

    Revert "Code_task2"
    This reverts commit 828e8979c66cc4a716d361ab054daedcc6fa0f63.

--- Exam_3_pract/is_it_palindrom.py
+++
@@ -1,8 +0,0 @@
-def is_it_palindrome(word):
-    word_backwards=word[::-1]
-    if word==word_backwards:
-        print('It is a palindrome')
-    else:
-        print('It is not a palindrome')
-
-is_it_palindrome(input('Input the word:\n'))
